0d46a9ab89d9ddd215743ae5e44650b20f44b5f3
yu-gotcha <yu.gotcha@gmail.com>
2021-06-26 18:00:29 +0900

    2.4 두 번째 테스트 만들기

--- test/practice_2/ProfileTest.java
+++ test/practice_2/ProfileTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/* JUnit은 결정된 순서로 테스트 실행하지 않음 */
+/* 모든 테스트는 다른 테스트 결과에 영향을 받지 않음 */
+/* 각각 별도의 ProfileTest 인스턴스 생성 */
+
 class ProfileTest {
 
     @Test
@@ -24,5 +28,24 @@ class ProfileTest {
         //Assert 단언
         assertFalse(matches);
     }
-    
+
+    @Test
+    void matchAnswersTrueForAnyDontCareCriteria() {
+        //Arrange 준비
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got milk?");
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        //Action 실행
+        boolean matches = profile.matches(criteria);
+
+        //Assert 단언
+        assertTrue(matches);
+    }
+
 }

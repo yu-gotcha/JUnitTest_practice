990352c7957b12c8e9eb9b623675f5a6c89866cd
yu-gotcha <yu.gotcha@gmail.com>
2021-06-26 16:04:31 +0900

    JUnit 기본: 첫 번째 테스트 통과

---
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,13 @@
+package iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreCollectionTest {
+
+    @Test
+    void arithmeticMean() {
+        fail("Not yet implemented");
+    }
+}
